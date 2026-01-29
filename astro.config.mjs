d4407748b70670bcfc8c8b23640ef8257fd24a29
fatosidrizi <fatos.idrizi@gmail.com>
2026-01-29 22:23:05 +0100

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://fatosidrizi.github.io',
+  base: '/ConnexaLine-3',
   integrations: [tailwind()],
   output: 'static',
   build: {
